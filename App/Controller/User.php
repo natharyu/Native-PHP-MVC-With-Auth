1f4847dbaf35cfc0c1b2fc6a6a24b9c7b108511f
Natharyu <anthony.dewittelebrun@gmail.com>
2023-01-23 20:09:49 +0100

    login validation change

--- App/Controller/User.php
+++ App/Controller/User.php
@@ -1,40 +1,38 @@
-<?php namespace App\Controller;
+<?php
+
+namespace App\Controller;
 
 use App\Model\Users;
 use App\Model\Session;
 
 class User
 {
-    public static function registerValidate() :void
+    public static function registerValidate(): void
     {
         $users = Users::all();
         $currentValues = self::getCurrentValues($_POST['username'], $_POST['email'], $_POST['password']);
-        $results= self::validateResults($users, $_POST['username'], $_POST['email'], $_POST['password'], $_POST['password-confirm']);
+        $results = self::validateResults($users, $_POST['username'], $_POST['email'], $_POST['password'], $_POST['password-confirm']);
 
-        if(isset( $_POST['agree']))
-        {
+        if (isset($_POST['agree'])) {
             $results['agree'] = "success";
-        }
-        else
-        {
+        } else {
             $results['agree'] = "Please accept the agreement.";
         }
         self::errorValidatation($results, $currentValues, 'register');
     }
 
-    public static function getCurrentValues(String $username = null, String $email = null, String $password = null, String $role = null) :array
+    public static function getCurrentValues(String $username = null, String $email = null, String $password = null, String $role = null): array
     {
         $currentValues['username'] = $username;
         $currentValues['email'] = $email;
-        if($password != null)
-        {
-            $currentValues['password'] = password_hash( $password, PASSWORD_BCRYPT );
+        if ($password != null) {
+            $currentValues['password'] = password_hash($password, PASSWORD_BCRYPT);
         }
         $currentValues['role'] = $role;
         return $currentValues;
     }
 
-    public static function validateResults(Array $users, String $username = null, String $email = null, String $password = null, String $passwordConfirm = null) :array
+    public static function validateResults(array $users, String $username = null, String $email = null, String $password = null, String $passwordConfirm = null): array
     {
         $results['username'] = self::usernameValidate($users, $username);
         $results['email'] = self::emailValidate($users, $email);
@@ -43,19 +41,15 @@ class User
         return $results;
     }
 
-    public static function errorValidatation(Array $results, Array $currentValues, String $route) :void
+    public static function errorValidatation(array $results, array $currentValues, String $route): void
     {
-        foreach($results as $result => $value)
-        {
-            if($value != "success")
-            {
+        foreach ($results as $result => $value) {
+            if ($value != "success") {
                 $error[$result] = $value;
             }
         }
-        if(!isset($error) || $error === null)
-        {
-            switch($route)
-            {
+        if (!isset($error) || $error === null) {
+            switch ($route) {
                 case 'register':
                     self::addUser($currentValues);
                     break;
@@ -63,10 +57,8 @@ class User
                     self::userLogin($currentValues);
                     break;
             }
-        }
-        else{
-            switch($route)
-            {
+        } else {
+            switch ($route) {
                 case 'register':
                     array_pop($currentValues);
                     $view = './App/Views/Home/Session/Register.php';
@@ -81,122 +73,85 @@ class User
         }
     }
 
-    public static function usernameValidate(Array $users, String $username) :string
+    public static function usernameValidate(array $users, String $username): string
     {
-        try
-        {
-            if(isset( $username ) && !empty( $username ))
-            {
-                $existingUser =[];           
-                foreach ($users as $user){
+        try {
+            if (isset($username) && !empty($username)) {
+                $existingUser = [];
+                foreach ($users as $user) {
                     $existingUser[] = $user['username'];
                 }
-                if(in_array($username, $existingUser))
-                {
-                    throw new \Exception( 'This username already exist.');
-                }
-                else
-                {
+                if (in_array($username, $existingUser)) {
+                    throw new \Exception('This username already exist.');
+                } else {
                     return 'success';
                 }
+            } else {
+                throw new \Exception('Username field is required.');
             }
-            else
-            {
-                throw new \Exception( 'Username field is required.');
-            }
-        }
-        catch( \Exception $exeption )
-        {
+        } catch (\Exception $exeption) {
             return $exeption->getMessage();
         }
     }
 
-    public static function emailValidate(Array $users, String $email) :string
+    public static function emailValidate(array $users, String $email): string
     {
-        try
-        {
-            if(isset( $email ) && !empty( $email ))
-            {
-                if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-                {
+        try {
+            if (isset($email) && !empty($email)) {
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                     throw new \Exception('Invalid email format.');
                 }
-                $existingEmail =[];
-                foreach ($users as $user){
+                $existingEmail = [];
+                foreach ($users as $user) {
                     $existingEmail[] = $user['email'];
                 }
-                if(in_array($email, $existingEmail))
-                {
+                if (in_array($email, $existingEmail)) {
                     throw new \Exception('This email is already taken.');
-                }
-                else
-                {
+                } else {
                     return 'success';
                 }
+            } else {
+                throw new \Exception('Email field is required.');
             }
-            else
-            {
-                throw new \Exception( 'Email field is required.');
-            }
-        }
-        catch( \Exception $exeption )
-        {
+        } catch (\Exception $exeption) {
             return $exeption->getMessage();
         }
     }
 
-    public static function passwordValidate(String $password) :string
+    public static function passwordValidate(String $password): string
     {
-        try
-        {
-            if(isset( $password ) && !empty( $password ))
-            {
+        try {
+            if (isset($password) && !empty($password)) {
                 $uppercase = preg_match('@[A-Z]@', $password);
                 $lowercase = preg_match('@[a-z]@', $password);
                 $number    = preg_match('@[0-9]@', $password);
                 $specialChars = preg_match('@[^\w]@', $password);
-                if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8)
-                {
+                if (!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
                     throw new \Exception('Password may have 8 min characters, an uppercase, a lowercase, a number & a special character.');
-                }
-                else
-                {
+                } else {
                     return 'success';
                 }
+            } else {
+                throw new \Exception('Password field is required.');
             }
-            else
-            {
-                throw new \Exception( 'Password field is required.');
-            }
-        }
-        catch( \Exception $exeption )
-        {
+        } catch (\Exception $exeption) {
             return $exeption->getMessage();
         }
     }
 
-    public static function passwordConfirmValidate(String $password, String $passwordConfirm) :string
+    public static function passwordConfirmValidate(String $password, String $passwordConfirm): string
     {
-        try
-        {
-        if(isset( $passwordConfirm ) && !empty( $passwordConfirm ))
-        {
-            if($password === $passwordConfirm)
-            {
-                return 'success';
+        try {
+            if (isset($passwordConfirm) && !empty($passwordConfirm)) {
+                if ($password === $passwordConfirm) {
+                    return 'success';
+                } else {
+                    throw new \Exception('Passwords does not match');
+                }
+            } else {
+                throw new \Exception('Password confirm field is required.');
             }
-            else
-            {
-                throw new \Exception( 'Passwords does not match');
-            }
-        }
-        else
-        {
-            throw new \Exception( 'Password confirm field is required.');
-        }
-        }
-        catch( \Exception $exeption )
-        {
+        } catch (\Exception $exeption) {
             return $exeption->getMessage();
         }
     }
@@ -207,36 +162,28 @@ class User
         Users::add([$values['username'], $values['email'], $values['password'], $sessionKey, 'USER']);
 
         $success = "Account created successfully ! You can connect yourself now.";
-        header("Location: /login?success=". $success);
+        header("Location: /login?success=" . $success);
         exit();
     }
 
-    public static function loginValidate() :void
+    public static function loginValidate(): void
     {
-        if( isset( $_POST['username'] ) && !empty( $_POST['username'] ) &&
-            isset( $_POST['password'] ) && !empty( $_POST['password'] ))
-        {
+        if (
+            isset($_POST['username']) && !empty($_POST['username']) &&
+            isset($_POST['password']) && !empty($_POST['password'])
+        ) {
             $user = Users::findByUsername($_POST['username']);
-            if($user)
-            {
+            if ($user) {
                 $results['username'] = 'success';
 
-                if( password_verify( $_POST['password'], $user['password'] ))
-                        {
-                            
-                        }
-                        else
-                        {
-                            $results['password'] = 'Wrong password.';
-                        }
-            }
-            else
-            {
+                if (password_verify($_POST['password'], $user['password'])) {
+                } else {
+                    $results['password'] = 'Wrong password.';
+                }
+            } else {
                 $results['username'] = 'No user found with this username.';
             }
-        }
-        else
-        {
+        } else {
             $results['username'] = 'Username field is required';
             $results['password'] = 'Password field is required';
         }
@@ -245,24 +192,24 @@ class User
         self::errorValidatation($results, $currentValues, 'login');
     }
 
-    public static function userLogin($currentValues) :void
+    public static function userLogin($currentValues): void
     {
         $user = Users::findByUsername($currentValues['username']);
         $sessionKey = sha1(uniqid());
-        
+
         $newUserData = [
             'session_key' => $sessionKey,
             'username' => $user['username'],
         ];
-        Users::updateUser( $newUserData, $user['id'] );
+        Users::updateUser($newUserData, $user['id']);
         Session::setSession($currentValues['username'], $user['id'], $sessionKey);
 
         header('Location: /');
         exit();
     }
 
-    
-    public static function logout() :void
+
+    public static function logout(): void
     {
         Session::closeSession();
 
